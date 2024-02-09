d27c3e236e5fcc1d47135ee16d5451e2f565fc3a
Ob55 <brian55mwangi@gmail.com>
2024-02-09 11:15:40 +0300

    done assigment

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,35 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); 
+    console.log("Submitted:", firstName, lastName);
+  }
+
   return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="firstName">First Name:</label>
+      <input
+        type="text"
+        id="firstName"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <label htmlFor="lastName">Last Name:</label>
+      <input
+        type="text"
+        id="lastName"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
       <button type="submit">Submit</button>
     </form>
   );
